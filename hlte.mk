d4f508bb5523ddfb292b7c17660619ab4a51bf77
Mahmoud Adel Ahmed <godfather.x64@gmail.com>
2016-08-28 11:18:08 +0200

    delete com.google.android.gms.persistent killing script
    form commit 02844590c75cd6933f7bd76d6511e9fc1d6b4b76
    
    was causing the reboots and root reset problems

--- hlte.mk
+++ hlte.mk
@@ -34,9 +34,9 @@ TARGET_SCREEN_WIDTH := 1080
 SCREEN_RATIO_PROPORTIONATE := true
 TARGET_SCREEN_ASPECT_RATIO := 16by9
 
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -137,7 +137,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.crda.sh \
-    init.kgms.sh \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -42,14 +42,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.kgms.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.kgms.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.crda.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
