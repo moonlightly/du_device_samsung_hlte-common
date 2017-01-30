c2d42c408db7e4d50b9236d92c01c570344bc4db
Mahmoud Adel <godfather.x64@gmail.com>
2017-01-30 22:29:01 +0200

    build only for N9005

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hlte hltetmo hltespr hlteusc hltevzw,$(TARGET_DEVICE)),)
+ifneq ($(filter hlte,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
